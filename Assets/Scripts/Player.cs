cbcc844ef9f77a25cf57e0f5962e9ec6631db74a
Aleksander Rurarz <alex@lichthund.com>
2019-12-13 16:47:41 +0100

    Make infinite run scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,10 @@ namespace DefaultNamespace
     public class Player : MonoBehaviour
     {
         [SerializeField] private float speed = 0.2f;
+        [SerializeField] private float MinShakeInterval = 0.2f;
+        [SerializeField] private float sqrShakeDetectionThreshold = 3.6f;
+        private float timeSinceLastShake;
+
         public void Move(Vector3 moveVector)
         {
             transform.position += new Vector3(moveVector.x, 0, 0);
@@ -14,6 +18,13 @@ namespace DefaultNamespace
         private void Update()
         {
             transform.position += new Vector3(0, 0, speed);
+            
+            if (Input.acceleration.sqrMagnitude >= sqrShakeDetectionThreshold
+                && Time.unscaledTime >= timeSinceLastShake + MinShakeInterval)
+            {
+                Debug.Log("shake");
+                timeSinceLastShake = Time.unscaledTime;
+            }
         }
     }
 }
--- Assets/Scripts/tileManager.cs
+++ Assets/Scripts/tileManager.cs
@@ -11,11 +11,14 @@ public class tileManager : MonoBehaviour
   private int amntOfTiles = 3;
   private float safeZone = 15.0f;
   private int lastPrefabInd = 0;
+  private GameManager gameManager;
 
   private List<GameObject> activeTiles;
     // Start is called before the first frame update
     private void Start()
     {
+      gameManager = GameManager.Instance;
+      
       activeTiles = new List<GameObject>();
       playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 
@@ -41,6 +44,23 @@ public class tileManager : MonoBehaviour
       go.transform.position = Vector3.forward*spawnZ;
       spawnZ += tileLength;
 
+      int i = 0;
+      foreach (Transform child in go.gameObject.transform)
+      {
+        gameManager.tracksPositions[i] = child;
+        i++;
+
+        if (i > 2)
+        {
+          break;
+        }
+      }
+      
+      //TODO hack XD
+      var tmp = gameManager.tracksPositions[0];
+      gameManager.tracksPositions[0] = gameManager.tracksPositions[1];
+      gameManager.tracksPositions[1] = tmp;
+      
       activeTiles.Add(go);
     }
 
