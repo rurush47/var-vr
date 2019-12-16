9d8834d191154ab295cea3762306eabaf177902b
Aleksander Rurarz <alex@lichthund.com>
2019-12-16 16:40:46 +0100

    Change buttons color when changing direction

---
+++ Assets/Scripts/ChangeDirButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeDirButton : MonoBehaviour
+{
+    [SerializeField] private Color onSelectColor = Color.yellow;
+    private Image buttonImage;
+    private void Start()
+    {
+        buttonImage = GetComponent<Image>();
+    }
+
+    public void OnSelect()
+    {
+        buttonImage.color = onSelectColor;
+    }
+
+    public void OnExit()
+    {
+        buttonImage.color = Color.white;
+    }
+}
